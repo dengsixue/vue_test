add0fcfbfd5dcf3ce951bd6f250c42742baedd85
Dengsixue <450292411@qq.com>
2021-01-13 22:06:26 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <button>{{ message }} </button>
+  </div>
+</template>
+
+<script>
+var vm=new Vue({
+  el:'#app',
+  data () {
+    msg:'我是那妞'
+  }
+})
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+  width: auto;
+  height: 30px;
+}
+
+</style>
